6102e75a0fa634b87dc6aef0d25beb6700853caf
wyqzl <wuyuqian19950217@qq.com>
2015-12-09 21:03:26 +0800

    链表
    创建结构体，定义student类的指针。定义p,q,head 指针在student类中。h动态建立一个存在的结构体
    ，把头节点赋值为0，然后把地址赋值给p指针。用for循环动态分配4个结构体的存储空间，分别输入4个数据域的姓名和年龄。将0赋值给p指针，然后通过
    while循环判断p是否等于零来输出结果

--- link list/main.cpp
+++ link list/main.cpp
@@ -7,9 +7,33 @@
 //
 
 #include <iostream>
+struct student
+{
+    char name[100];
+    int age;
+    student *next;
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int i;
+    student *p,*q,*head;
+    head=new student;
+    head->next=0;
+    p=head;
+    for(i=0;i<4;i++)
+    {
+        q=new student;
+        p->next=q;
+        q->next=0;
+        scanf("%s %d",q->name,&(q->age));
+        p=q;
+    }
+    p=head->next;
+    while(p!=0)
+    {
+        printf("%s   %d\n",p->name,p->age);
+        p=p->next;
+    }
     return 0;
 }
